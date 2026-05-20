c8ebae7dad139db1ad82dbacafde70cbb3af50a2
Leon3352 <robuk.leonid@gmail.com>
2026-05-20 18:56:36 +0500

    Update hq-srv.sh

--- hq-srv.sh
+++ hq-srv.sh
@@ -1,34 +1,30 @@
 #!/bin/bash
-# НАСТРОЙКА HQ-SRV
+IFACE="ens18"
 
-IFACE="ens20"  # если у вас ens18 — поменяйте здесь один раз
+mkdir -p /etc/net/ifaces/ens18
 
-mkdir -p /etc/net/ifaces/$IFACE
-
-cat > /etc/net/ifaces/$IFACE/options <<'EOF'
+cat > /etc/net/ifaces/ens18/options <<'EOF'
 DISABLE=no
 TYPE=eth
 BOOTPROTO=static
 CONFIG_IPV4=yes
 EOF
 
-cat > /etc/net/ifaces/$IFACE/ipv4address <<'EOF'
+cat > /etc/net/ifaces/ens18/ipv4address <<'EOF'
 192.168.1.10/27
 EOF
 
-cat > /etc/net/ifaces/$IFACE/ipv4route <<'EOF'
+cat > /etc/net/ifaces/ens18/ipv4route <<'EOF'
 default via 192.168.1.1
 EOF
 
 systemctl restart network
 
-# Пользователь
 useradd sshuser -u 2026
 echo "sshuser:P@ssw0rd" | chpasswd
 echo "WHEEL_USERS ALL=(ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers
 gpasswd -a sshuser wheel
 
-# SSH
 echo "Authorized access only" > /etc/openssh/banner
 sed -i 's/^#*Port .*/Port 2026/' /etc/openssh/sshd_config
 sed -i 's/^#*PasswordAuthentication .*/PasswordAuthentication yes/' /etc/openssh/sshd_config
@@ -39,10 +35,9 @@ Banner /etc/openssh/banner
 EOF
 systemctl restart sshd
 
-# DNS + DNSmasq
 systemctl disable --now bind 2>/dev/null
 echo "nameserver 8.8.8.8" > /etc/resolv.conf
-apt-get update && apt-get install -y dnsmasq
+apt-get update && apt-get install dnsmasq -y
 
 cat > /etc/dnsmasq.conf <<'EOF'
 no-resolv
@@ -66,5 +61,4 @@ EOF
 echo "192.168.1.1	hq-rtr.au-team.irpo" >> /etc/hosts
 systemctl enable --now dnsmasq
 
-# Часовой пояс
 timedatectl set-timezone Asia/Yekaterinburg
