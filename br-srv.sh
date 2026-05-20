d68c4748a7ad6aa645c08d1abdceb82398a63e23
Leon3352 <robuk.leonid@gmail.com>
2026-05-20 18:57:15 +0500

    Update br-srv.sh

--- br-srv.sh
+++ br-srv.sh
@@ -1,34 +1,30 @@
 #!/bin/bash
-# НАСТРОЙКА BR-SRV
+IFACE="ens18"
 
-IFACE="ens20"  # поменяйте на ens18 если нужно
+mkdir -p /etc/net/ifaces/ens18
 
-mkdir -p /etc/net/ifaces/$IFACE
-
-cat > /etc/net/ifaces/$IFACE/options <<'EOF'
+cat > /etc/net/ifaces/ens18/options <<'EOF'
 DISABLE=no
 TYPE=eth
 BOOTPROTO=static
 CONFIG_IPV4=yes
 EOF
 
-cat > /etc/net/ifaces/$IFACE/ipv4address <<'EOF'
+cat > /etc/net/ifaces/ens18/ipv4address <<'EOF'
 192.168.3.10/28
 EOF
 
-cat > /etc/net/ifaces/$IFACE/ipv4route <<'EOF'
+cat > /etc/net/ifaces/ens18/ipv4route <<'EOF'
 default via 192.168.3.1
 EOF
 
 systemctl restart network
 
-# Пользователь
 useradd sshuser -u 2026
 echo "sshuser:P@ssw0rd" | chpasswd
 echo "WHEEL_USERS ALL=(ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers
 gpasswd -a sshuser wheel
 
-# SSH
 echo "Authorized access only" > /etc/openssh/banner
 sed -i 's/^#*Port .*/Port 2026/' /etc/openssh/sshd_config
 sed -i 's/^#*PasswordAuthentication .*/PasswordAuthentication yes/' /etc/openssh/sshd_config
@@ -39,5 +35,4 @@ Banner /etc/openssh/banner
 EOF
 systemctl restart sshd
 
-# Часовой пояс
 timedatectl set-timezone Asia/Yekaterinburg
