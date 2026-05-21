1c475007e2c31451eb86664e528e906375a50e37
Leon3352 <robuk.leonid@gmail.com>
2026-05-22 03:53:32 +0500

    Update hq-srv.sh

--- hq-srv.sh
+++ hq-srv.sh
@@ -1,32 +1,25 @@
-#!/bin/bash
-IFACE="ens18"
-
+hostnamectl set-hostname hq-srv.au-team.irpo; exec bash
 mkdir -p /etc/net/ifaces/ens18
-
 cat > /etc/net/ifaces/ens18/options <<'EOF'
 DISABLE=no
 TYPE=eth
 BOOTPROTO=static
 CONFIG_IPV4=yes
 EOF
-
 cat > /etc/net/ifaces/ens18/ipv4address <<'EOF'
 192.168.1.10/27
 EOF
-
 cat > /etc/net/ifaces/ens18/ipv4route <<'EOF'
 default via 192.168.1.1
 EOF
-
 systemctl restart network
-
+ip -c a
 useradd sshuser -u 2026
 echo "sshuser:P@ssw0rd" | chpasswd
 echo "WHEEL_USERS ALL=(ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers
 gpasswd -a sshuser wheel
-
 echo "Authorized access only" > /etc/openssh/banner
-sed -i 's/^#*Port .*/Port 2026/' /etc/openssh/sshd_config
+sed -i 's/^#*Port .*/Port 2014/' /etc/openssh/sshd_config
 sed -i 's/^#*PasswordAuthentication .*/PasswordAuthentication yes/' /etc/openssh/sshd_config
 cat >> /etc/openssh/sshd_config <<'EOF'
 AllowUsers sshuser
@@ -34,11 +27,11 @@ MaxAuthTries 2
 Banner /etc/openssh/banner
 EOF
 systemctl restart sshd
-
 systemctl disable --now bind 2>/dev/null
 echo "nameserver 8.8.8.8" > /etc/resolv.conf
 apt-get update && apt-get install dnsmasq -y
-
+systemctl enable --now dnsmasq.service
+systemctl status dnsmasq.service
 cat > /etc/dnsmasq.conf <<'EOF'
 no-resolv
 domain=au-team.irpo
@@ -50,15 +43,14 @@ address=/hq-srv.au-team.irpo/192.168.1.10
 address=/hq-cli.au-team.irpo/192.168.2.10
 address=/br-rtr.au-team.irpo/192.168.3.1
 address=/br-srv.au-team.irpo/192.168.3.10
-address=/docker.au-team.irpo/172.16.1.1
-address=/web.au-team.irpo/172.16.2.1
+address=/docker.au-team.irpo/172.16.70.1
+address=/web.au-team.irpo/172.16.80.1
 
 ptr-record=1.1.168.192.in-addr.arpa,hq-rtr.au-team.irpo
 ptr-record=10.1.168.192.in-addr.arpa,hq-srv.au-team.irpo
 ptr-record=10.2.168.192.in-addr.arpa,hq-cli.au-team.irpo
 EOF
-
 echo "192.168.1.1	hq-rtr.au-team.irpo" >> /etc/hosts
-systemctl enable --now dnsmasq
-
+systemctl restart dnsmasq.service
+ping google.com
 timedatectl set-timezone Asia/Yekaterinburg
